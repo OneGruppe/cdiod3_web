c395b740170fdb8dbbdcc25ae821b1f171c95e2d
Mathias Fager <mathias_1337@hotmail.com>
2018-04-20 16:31:11 +0200

    CreateUser fixed

--- src/functionality/Functionality.java
+++ src/functionality/Functionality.java
@@ -45,13 +45,11 @@ public class Functionality //implements IFunctionality{
 		List<UserDTO> DTOList = null;
 		try {
 			DTOList = dao.getUserList();
-			System.out.println("Shit works yo");
 		} catch (DALException e1) {
 			e1.printStackTrace();
 		}
 
 		for (int j = 0; j < DTOList.size(); j++) {
-			System.out.println("Checking if " + name + " is equal to " + DTOList.get(j).getUserName());
 			if (DTOList.get(j).getUserName().equals(name) || DTOList.get(j).getIni().equals(ini) || DTOList.get(j).getCpr().equals(cpr)) {
 				return "Username, ini or CPR already exists";
 			}
@@ -77,8 +75,17 @@ public class Functionality //implements IFunctionality{
 			System.out.println(role);
 		}
 		System.out.println("\n ----***** FUNK: CreateUser end *****----");
-
-		if (cpr.matches("\\d{6}\\-\\d{4}") && !name.equals(null) && !password.equals(null) && !ini.equals(null) && password.length() <= 20) {
+		if (!cpr.matches("\\d{6}\\-\\d{4}")) {return "CPR does not match 6 digits dash 4 digits";}
+		if (!name.matches("[a-åA-Å0-9]{4,20}$")) {return "Username does not match a-å, A-Å or 0-9 while being between 4 and 20 characters";}
+		if (!password.matches("[a-åA-Å0-9]{4,20}$")) {return "Password does not match a-å, A-Å or 0-9 while being between 4 and 20 characters";}
+		if (!ini.matches("[a-åA-Å]{1,3}$")) {return "Initials does not match a-å or A-Å while being bewteen 1 and 3 characters";}
+		
+		System.out.println("username " + name.matches("[a-åA-Å0-9]{4,20}$"));
+		System.out.println("password " + password.matches("[a-åA-Å0-9]{4,20}$"));
+		System.out.println("ini " + ini.matches("[a-åA-Å]{1,3}$"));
+		System.out.println("cpr " + cpr.matches("\\d{6}\\-\\d{4}"));
+		
+		if (cpr.matches("\\d{6}\\-\\d{4}") && name.matches("[a-åA-Å0-9]{4,20}$") && password.equals("[a-åA-Å0-9]{4,20}$") && ini.equals("[a-åA-Å]{1,3}$")) {
 			UserDTO newUser = new UserDTO(0, name, password, ini, cpr, roleList);
 			try {
 				dao.createUser(newUser);
