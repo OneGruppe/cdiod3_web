15c2ed5413707ef0b2f6b15cd517805dbcc2baea
Niklaes Jacobsen <niklaes@gmail.com>
2018-04-20 16:22:06 +0200

    WEB: updateUser - version 0.1

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -48,5 +48,16 @@ $(document).ready(function() {
 		})
 
 	});
-
+	$("#findUser").click(function() {
+		$.ajax({
+			url:"rest/functionality/showUser",
+			data: $('#targetUsername').serialize(),
+			contenttype: "application/x-ww-form-urlencoded",
+			method: "POST",
+			success:function(data) {
+				alert(data);
+				$(".loadToDiv").load("createUser.html");
+			}
+		})
+	})
 });
