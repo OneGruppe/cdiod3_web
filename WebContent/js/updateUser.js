ebdb4ad61b1ce4a0654b5912e5810de57373916c
Sebastian <s170423@student.dtu.dk>
2018-04-22 20:50:49 +0200

    WEB: Tilføjet JS for changeUser

--- WebContent/js/updateUser.js
+++ WebContent/js/updateUser.js
@@ -3,18 +3,33 @@
  */
 
 $(document).ready(function() {
-	
-	$("#findUserToUpdate").click(function() {
-		
+
+	/*$("#findUserToUpdate").click(function() {
+
 		$.ajax({
-			url:"rest/functionality/showUser",
-			data: $('#targetUsername').serialize(),
-			contenttype: "application/x-ww-form-urlencoded",
-			method: "POST",
-			success:function(data) {
-				alert(data);
+			url : "rest/functionality/showUser",
+			data : $('#targetUsername').serialize(),
+			contenttype : "application/x-ww-form-urlencoded",
+			method : "GET",
+			success : function(data) {
+
 			}
 		})
-				return false;
+		return false;
+	});*/
+
+	$("#actuallyUpdatingAUser").click(function() {
+
+		$.ajax({
+			url : "rest/functionality/updateUser",
+			data : $('.updateUserContainer').serialize(),
+			contenttype : "application/x-ww-form-urlencoded",
+			method : "POST",
+			success : function(data) {
+				alert(data);
+				$(".loadToDiv").load("html/updateUser.html");
+			}
+		})
+		return false;
 	});
 });
